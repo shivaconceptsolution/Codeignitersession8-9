7f5964654ac69d04e0f8a9cc7356e9fbad70bdcd
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-09-11 09:26:34 +0530

    Add files via upload

--- controllers/Login.php
+++ controllers/Login.php
@@ -5,7 +5,9 @@ class Login extends CI_Controller
     function index()
     {
 
-    	$this->load->view('loginview');
+        $data= array('a'=>$this->input->cookie('cuid'),'b'=>$this->input->cookie('cpwd'));
+
+    	$this->load->view('loginview',$data);
     }
 
     function logincode()
@@ -17,7 +19,16 @@ class Login extends CI_Controller
     	$c=$this->loginmodel->login($user,$pass);
     	if($c>0)
     	{
-    		$this->session->set_userdata('uid',$user);
+            $this->session->set_userdata('uid',$user);
+            if($this->input->post('chk1')!=null)
+            {
+
+                
+            $this->input->set_cookie('cuid',$user,3600);
+            $this->input->set_cookie('cpwd',$pass,3600);
+            }
+    		
+
     		redirect('student/index');
     	}
     	else
